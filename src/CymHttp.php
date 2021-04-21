275e52af29a2a7447892785b6b6d1687b2c0c759
cym504875043 <504875043@qq.com>
2021-04-21 22:47:04 +0800

    add qrcode

---
+++ src/CymHttp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CymPhptools;
+
+class CymHttp{
+    /**
+     * 
+     */
+    public function get(){
+
+    }
+    /**
+     * 
+     */
+    public function post(){
+
+    }
+    /**
+     * 
+     */
+    public function getPuls(){
+
+    }
+    /**
+     * 
+     */
+    public function postPuls(){
+
+    }
+}
