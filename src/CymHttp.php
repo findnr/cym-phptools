cb15e5cfda5a628c6011496d16b8610f75cae966
findnr <504875043@qq.com>
2021-05-17 11:24:44 +0800

    add post to arr

--- src/CymHttp.php
+++ src/CymHttp.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * @Author: 程英明
+ * @Date: 2021-04-23 15:39:24
+ * @LastEditTime: 2021-05-17 11:24:08
+ * @LastEditors: 程英明
+ * @Description: 
+ * @FilePath: \cym-phptools\src\CymHttp.php
+ * QQ:504875043@qq.com
+ */
 
 namespace CymPhptools;
 
@@ -24,7 +33,7 @@ class CymHttp
             'form_params' => $data
         ]);
         $data['info'] = $response;
-        $data['data'] = (string) $response->getBody();
+        $data['data'] = json_decode((string) $response->getBody());
         return $data;
     }
     /**
